ec207c6cea6f51d21b0b66b724a83c321e8d7e36
eunsookim1 <92eunsoo@gmail.com>
2023-05-27 01:50:34 +0900

    refreshing char counter upon hitting submit button

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,29 +3,27 @@
 //   $('#counter').text(len + ' characters');
 // };
 
-$('document').ready(function(e) { //Calls up when the DOM (document object model) is ready
+$("document").ready(function (e) {
+  //Calls up when the DOM (document object model) is ready
+  const maxChars = 140;
   //alert("hello");
 
   //TextBox Event
   // eslint-disable-next-line space-before-blocks
-  $('#tweet-text').keypress(function(e){
-    let maxChars = 140;
-    let tweetText = $('#tweet-text').val();
+  $("#tweet-text").keypress(function (e) {
+    let tweetText = $("#tweet-text").val();
     let totalChars = tweetText.length;
     let remainingChars = maxChars - totalChars;
     //If the remaining characters are still greater than zero
     if (remainingChars > 0) {
-      $('#counter').text(remainingChars);
-      $('#counter').text(remainingChars).css("color", "#545149");
-    
+      $("#counter").text(remainingChars);
+      $("#counter").text(remainingChars).css("color", "#545149");
     } else {
-      $('#counter').text(remainingChars);
-      $('#counter').css("color", "red");
+      $("#counter").text(remainingChars);
+      $("#counter").css("color", "red");
     }
-
   });
 });
-
 
 // $("#tweet-text").on('click', function() {
 //   console.log(this); //The this keyword is a reference to the button
