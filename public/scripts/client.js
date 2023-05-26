9d7f38128f41b4364458871edae082a3dafe4efc
eunsookim1 <123800349+eunsookim1@users.noreply.github.com>
2023-05-27 01:54:20 +0900

    Update client.js / code cleaned & removed comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,43 +5,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// import { format } from "timeago.js";
-
-// const days = function (date) {
-//   const timeWritten = new Date(date);
-//   const today = new Date();
-
-//   const difference = today - timeWritten;
-//   const totalDays = Math.ceil(difference / (1000 * 3600 * 24));
-//   return totalDays;
-// };
-
 $("document").ready(function () {
   $(".error-message").hide();
-  // const data = [
-  //   {
-  //     user: {
-  //       name: "Newton",
-  //       avatars: "https://i.imgur.com/73hZDYK.png",
-  //       handle: "@SirIsaac",
-  //     },
-  //     content: {
-  //       text: "If I have seen further it is by standing on the shoulders of giants",
-  //     },
-  //     created_at: 1461116232227,
-  //   },
-  //   {
-  //     user: {
-  //       name: "Descartes",
-  //       avatars: "https://i.imgur.com/nlhLi3I.png",
-  //       handle: "@rd",
-  //     },
-  //     content: {
-  //       text: "Je pense , donc je suis",
-  //     },
-  //     created_at: 1461113959088,
-  //   },
-  // ];
 
   const renderTweets = function (tweets) {
     $("#tweets-container").empty();
@@ -129,21 +94,3 @@ $("document").ready(function () {
 
   loadTweets();
 });
-
-// // Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
-// Need to input the new-tweet into data
-// call the function above to push into the ordered tweet containers
-
-// $(function () {
-//   const $button = $('#load-more-tweet');
-//   $button.on('click', function () {
-//     $.ajax(${'#tweet-text'})
-//   })
-// })
-
-// $("#load-new-tweet").bind("click", function () {
-//   alert("User clicked on 'submit'");
-// });
